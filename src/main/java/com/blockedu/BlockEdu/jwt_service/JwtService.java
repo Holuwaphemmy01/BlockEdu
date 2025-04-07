c4823f82925b2e9e0b195b5dedbd549595d8cafd
Holuwaphemmy <holuwaphemmy001@gmail.com>
2025-04-07 19:20:13 +0100

    jwt service ongoing

--- src/main/java/com/blockedu/BlockEdu/jwt_service/JwtService.java
+++ src/main/java/com/blockedu/BlockEdu/jwt_service/JwtService.java
@@ -1,4 +1,78 @@
 package com.blockedu.BlockEdu.jwt_service;
 
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import io.jsonwebtoken.Jwts;
+
+import static jdk.jfr.internal.EventWriterKey.getKey;
+
+
+@Service
 public class JwtService {
+
+    private String secretKey;
+
+    public JwtService() {
+        KeyGenerator keyGenerator = null;
+
+        try{
+            keyGenerator = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey key = keyGenerator.generateKey();
+            secretKey = Base64.getEncoder().encodeToString(key.getEncoded());
+        }
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+60*60*100))
+                .and()
+                .signWith(getKey())
+                .compact();
+    }
+
+    private SecretKey getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUsername(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String jwtToken, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(jwtToken);
+        return claimsResolver.apply(claims);
+
+    }
+
+    private Claims extractAllClaims(String jwtToken) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(jwtToken)
+                .getPayload();
+    }
+
 }

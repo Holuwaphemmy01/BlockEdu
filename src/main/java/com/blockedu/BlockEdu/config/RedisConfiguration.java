d283a6fab23e6622c571f94a503d5f3563830919
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2025-04-24 11:21:16 +0100

    refactoring

--- src/main/java/com/blockedu/BlockEdu/config/RedisConfiguration.java
+++ src/main/java/com/blockedu/BlockEdu/config/RedisConfiguration.java
@@ -1,41 +1,31 @@
-package com.blockedu.BlockEdu.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-@Configuration
-public class RedisConfiguration {
-
+//package com.blockedu.BlockEdu.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//
+//@Configuration
+//public class RedisConfiguration {
+//
 //    @Bean
-//    public RedisTemplate <String, String> redisTemplate(RedisConnectionFactory factory) {
-//        RedisTemplate <String, String> template = new RedisTemplate<>();
+//    public RedisTemplate<String, byte[]> redisTemplate(RedisConnectionFactory factory) {
+//        RedisTemplate<String, byte[]> template = new RedisTemplate<>();
 //        template.setConnectionFactory(factory);
 //        template.setKeySerializer(new StringRedisSerializer());
-//        template.setHashKeySerializer(new StringRedisSerializer());
+//        template.setValueSerializer(new ByteArrayRedisSerializer());
+//        return template;
+//    }
+//
+//    @Bean
+//    public RedisTemplate<String, String> stringRedisTemplate(RedisConnectionFactory factory) {
+//        RedisTemplate<String, String> template = new RedisTemplate<>();
+//        template.setConnectionFactory(factory);
+//        template.setKeySerializer(new StringRedisSerializer());
 //        template.setValueSerializer(new StringRedisSerializer());
 //        return template;
 //    }
-
-    @Bean
-    public RedisTemplate<String, byte[]> redisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, byte[]> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new ByteArrayRedisSerializer());
-        return template;
-    }
-
-    @Bean
-    public RedisTemplate<String, String> stringRedisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-        return template;
-    }
-}
-
-
+//}
+//
+//

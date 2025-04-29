3cf7dd21da0798ccdbd85bfb77e8c67c8740f84b
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2025-04-29 11:23:58 +0100

    change pasword controller done

---
+++ src/main/java/com/blockedu/BlockEdu/controller/change_password/ChangePasswordController.java
@@ -0,0 +1,26 @@
+package com.blockedu.BlockEdu.controller.change_password;
+
+import com.blockedu.BlockEdu.data.dtos.request.ChangePasswordRequest;
+import com.blockedu.BlockEdu.data.dtos.response.ChangePasswordResponse;
+import com.blockedu.BlockEdu.service.student.change_password.ChangePasswordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class ChangePasswordController {
+
+    @Autowired
+    private ChangePasswordService changePasswordService;
+
+
+    @PostMapping("/changePassword")
+    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordRequest request){
+        ChangePasswordResponse response = changePasswordService.changePassword(request);
+        return ResponseEntity.ok(response);
+    }
+}
